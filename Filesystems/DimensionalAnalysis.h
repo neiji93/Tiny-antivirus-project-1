d7cc7993870ab6bedb603f507f701f8e7e4db21d
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-02-09 09:19:03 +0100

    Create DimensionalAnalysis.h

---
+++ Filesystems/DimensionalAnalysis.h
@@ -0,0 +1,9 @@
+
+
+//increase or decrease with slight code.
+//the best thing here will be to optimize files.
+//so as there are steel in hds, we can use ai.
+//then lets use kernel methods.
+
+class kernelMethodsAntitrust
+{}
