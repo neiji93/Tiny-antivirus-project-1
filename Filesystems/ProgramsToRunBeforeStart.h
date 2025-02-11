60ec45309d8a3a926a9023269713e5dcf4a55b65
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-02-11 14:38:44 +0100

    Create ProgramsToRunBeforeStart.h

---
+++ Filesystems/ProgramsToRunBeforeStart.h
@@ -0,0 +1,23 @@
+
+
+//with ai, it seems rapid because we dont really need latent variables models in all of our antivirus.
+//Nevertheless, we can add an extra part. But here, lets use genetic algorithms. Plus, to prevent this class to be
+//a scheduller, we can prevent optimizers. So, the tresholds in our filesysstems management classes will be secrets !
+//I didnt use isolates immediately and I didnt use patterns. Then, This class can communicate with even micro kernells.
+//But to do that, you will have to code your own methods...
+class PreviousHashesDropper
+{
+
+public:
+   //void sendSignalThreeTimes(....); //more secure because of our first particle conductor.
+   void unsafeSendSignalFourTimes(....); //better suited with ai.
+
+
+
+   void patchmeSrverGetCountIncommingSignals(....); //a usefull function ! So patch it ! Usefull twice
+                                                    //because we use it in a boot system. As previous signals count
+                                                    //will be hold, here we can have an unsecure but rapid first attempt
+                                                  //to check all ai classes or functions ! Those things must be sage !
+                                                  //So, which ai theories to use with genetic algorithlms ?
+                                                  //Let's use algorithms that are .....
+}
