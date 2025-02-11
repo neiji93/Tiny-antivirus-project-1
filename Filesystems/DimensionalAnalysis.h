b0229a45eafe0f855cb903f6d0aa1cf42aa22ce4
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-02-11 14:27:07 +0100

    Update DimensionalAnalysis.h

--- Filesystems/DimensionalAnalysis.h
+++ Filesystems/DimensionalAnalysis.h
@@ -5,5 +5,42 @@
 //so as there are steel in hds, we can use ai.
 //then lets use kernel methods.
 
-class kernelMethodsAntitrust
-{}
+class kernelMethodsAntitrust  //this class will contain only one method
+{
+public:
+  void patchMeSingleIntegralBehaviourDonNotCountMemoryLeaks(..); //suited to not use some OS intrinsics
+}
+
+
+
+//As single integrals can be useful and as we didnt use OS functions in the above class, 
+//we can use OS functions here. So as main formulas are equations in non memory management,
+//we can manage user space modules. It seems now that we do not trust micro kernells theories.
+//On the other hand, it also seems that we have something to do not trust user network.
+class NotMicroKernelUserSpaceModuleCoroutineBlockMethods
+{
+
+public:
+//we already deal with physical components.
+ //then, we can set a treshold to hold passive behaviour.
+//The interesting thing is that some ai tresholds can be the same as this treshold.
+  void aiPushCommandsMessageByUSBorEthernetLongTimelapseThreshold_Zero_dot_thirteen(...);  //physics. My stuff here again. 
+
+ //we can immediately set an ownership as the treshold above is not a big integer.
+  void getAccessToRouterIPsIfEquationParametersIsTheSameProtocol(....); 
+}
+
+
+//we can try to see if we can do try catch again in the futur messages...
+class FilesystemTryCatchMainMovementEquationSetSomeFourCompoenentsOfTheMainScheduller
+{
+
+public:
+  void partialMethodGetFourOrThreeHashes(...); //hashes can load programs.
+
+}
+
+
+
+
+
